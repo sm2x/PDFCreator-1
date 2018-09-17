fb76b58f599418b095678702f8d9c64013719ae3
Luke-Hodson <luke.hodson@bbs-actuaries.co.uk>
2018-09-17 16:25:32 +0100

    Some OO refactoring

--- PDFCreator/PDFGenerator.cs
+++ PDFCreator/PDFGenerator.cs
@@ -31,6 +31,10 @@ namespace PDFCreator
         float _paraOffset;
         float _horizontalOffset;
         DataAccess _dataAccess;
+        float _verticalPosition;
+        float _titleParaHeight;
+        float _titleParaWidth;
+        string _text;
 
 
         public PDFGenerator()
@@ -42,6 +46,7 @@ namespace PDFCreator
             _horizontalOffset = 130;
             _baptistBlue = new DeviceRgb(0, 176, 240);
             _dataAccess = new DataAccess(false);
+            _verticalPosition = _pageHeight;
             GenerateFilePath();
         }
 
@@ -50,7 +55,6 @@ namespace PDFCreator
             CreateNewPDF();
         }
 
-
         private void CreateNewPDF()
         {
             _writer = new PdfWriter(_path);
@@ -58,83 +62,32 @@ namespace PDFCreator
             _document = new Document(_pdf);
             _document.SetMargins(0, 100, 0, 100);
 
-            SpoolPageOne();               
+            SpoolPageOne();
+            SpoolPageTwo();
 
             _document.Close();
         }
 
         private void SpoolPageOne()
-        {
-            float titleParaHeight;
-            float titleParaWidth;
-            string text = "";
-            float verticalPosition = _pageHeight;
+        {                 
+            AddLogo();
+            _verticalPosition -= 220;
+            AddTitle(GetText(MergeField.Title));
 
-            //logo            
-            string strFilePath = @"\\bbs-actuaries\dfsdata\users\hodsonl\Visual Studio 2017\Projects\PDFCreator\PDFCreator\Images\baptist.png";
-            ImageData l = ImageDataFactory.Create(strFilePath);
-            Image img = new Image(l);
-            img.SetHeight(39);
-            img.SetWidth(110);
-            img.SetFixedPosition(_pageWidth - 180, _pageHeight - 65);
-            _document.Add(img);
+            _verticalPosition -= 30;
+            AddTitle(GetText(MergeField.EstimatedEmployerDebtAt));
 
-            //title            
-            text = GetText(MergeField.Title);
-            Paragraph title = new Paragraph(text);
-            titleParaHeight = 126;
-            titleParaWidth = _pageWidth;
-            title.SetFontSize(13);
-            title.SetBold();
-            title.SetTextAlignment(TextAlignment.CENTER);
-            title.SetFontColor(_baptistBlue);
-            title.SetUnderline();
-            title.SetWidth(titleParaWidth);
-            title.SetHeight(titleParaHeight);
-            verticalPosition -= 220;
-            title.SetFixedPosition((_pageWidth - titleParaWidth), verticalPosition, titleParaWidth);
-            _document.Add(title);            
-
-            //estimated employer debt
-            text = GetText(MergeField.EstimatedEmployerDebt);
-            Paragraph employerDebt = new Paragraph(text);
-            titleParaHeight = 126;
-            titleParaWidth = _pageWidth;
-            employerDebt.SetFontSize(13);
-            employerDebt.SetUnderline();
-            employerDebt.SetBold();
-            employerDebt.SetTextAlignment(TextAlignment.CENTER);
-            employerDebt.SetFontColor(_baptistBlue);
-            employerDebt.SetWidth(titleParaWidth);
-            employerDebt.SetHeight(titleParaHeight);
-            verticalPosition -= 30;
-            employerDebt.SetFixedPosition((_pageWidth - titleParaWidth), verticalPosition, titleParaWidth);
-            _document.Add(employerDebt);
-
-            //"Introduction"
-            text = "Introduction";
-            Paragraph intro = new Paragraph(text);
-            titleParaHeight = 126;
-            titleParaWidth = _pageWidth;
-            intro.SetFontSize(11);
-            intro.SetTextAlignment(TextAlignment.CENTER);
-            intro.SetFontColor(_baptistBlue);
-            intro.SetUnderline();
-            intro.SetBold();
-            intro.SetWidth(titleParaWidth);
-            intro.SetHeight(titleParaHeight);
-            verticalPosition -= 40;
-            intro.SetFixedPosition((_pageWidth - titleParaWidth), verticalPosition, titleParaWidth);
-            _document.Add(intro);
+            _verticalPosition -= 40;
+            AddTitle("Introduction");
 
             //List of items            
             List introList = new List(ListNumberingType.DECIMAL);                                    
-            titleParaWidth = _pageWidth - _horizontalOffset;
+            _titleParaWidth = _pageWidth - _horizontalOffset;
             introList.SetFontSize(10);
             introList.SetTextAlignment(TextAlignment.LEFT);
-            introList.SetWidth(titleParaWidth);
+            introList.SetWidth(_titleParaWidth);
             introList.SetMinHeight(150);
-            verticalPosition -= 300;
+            _verticalPosition -= 300;
 
             //Item 1
             Paragraph para = new Paragraph();
@@ -171,7 +124,7 @@ namespace PDFCreator
             item5SubList.Add("an employer debt only becomes due when an employer incurs a “cessation event”");
             item5SubList.Add("a cessation event normally only occurs when an employer stops employing any active members of the BPS");            
             item5SubList.SetFontSize(10);
-            item5SubList.SetFixedPosition((_pageWidth - titleParaWidth - 50), verticalPosition + 115, titleParaWidth);
+            item5SubList.SetFixedPosition((_pageWidth - _titleParaWidth - 50), _verticalPosition + 115, _titleParaWidth);
             _document.Add(item5SubList);
             
             //Item 6
@@ -185,7 +138,7 @@ namespace PDFCreator
             listItemSeven.SetPaddingBottom(10);
             introList.Add(listItemSeven);                     
 
-            introList.SetFixedPosition((_pageWidth - titleParaWidth - (_horizontalOffset / 2)), verticalPosition, titleParaWidth);
+            introList.SetFixedPosition((_pageWidth - _titleParaWidth - (_horizontalOffset / 2)), _verticalPosition, _titleParaWidth);
             _document.Add(introList);
 
             //nested list
@@ -195,26 +148,30 @@ namespace PDFCreator
             item7SubList.Add("The amount due could be higher or lower if your organisation is currently in a “period of grace” (a “period of grace” applies if you have had a cessation event and you do not currently employ an active member, but you have confirmed that you expect to take on a new employee who will become a BPS member soon).  As a period of grace can only be granted if an employer requests it, your organisation should be aware if a period of grace applies in your case.  If an employer in a period of grace does not take on a new active member before the end of a period of grace, that employer’s debt will be calculated based on the finances of the Scheme at the date of the cessation event rather than at a current date.");
             if (string.IsNullOrEmpty(previousCEWords))
             {
-                verticalPosition -= 140;
+                _verticalPosition -= 140;
             }
             else
             {
-                verticalPosition -= 160;
+                _verticalPosition -= 160;
             }
             
             item7SubList.SetFontSize(10);
-            item7SubList.SetFixedPosition((_pageWidth - titleParaWidth - 50), verticalPosition, titleParaWidth);
+            item7SubList.SetFixedPosition((_pageWidth - _titleParaWidth - 50), _verticalPosition, _titleParaWidth);
             _document.Add(item7SubList);
-
-
         }
 
-        #region helper methods
-        private void AddParagraph(Document doc, Paragraph paragraph)
+        private void SpoolPageTwo()
         {
-            doc.Add(paragraph);
+            //reset position tracker
+            _verticalPosition = _pageHeight;
+
+            NewPage();
+            AddLogo();
+            _verticalPosition -= 220;
+            AddTitle(GetText(MergeField.EstimatedEmployerDebt));
         }
 
+        #region helper methods      
         private void GenerateFilePath()
         {
             _path = @"\\bbs-actuaries\dfsdata\users\hodsonl\pdfs\test";
@@ -230,7 +187,7 @@ namespace PDFCreator
                 case MergeField.Title:
                     text = string.Format("The Baptist Pension Scheme (BPS) – {0}", _dataAccess.GetEmployerName());                    
                     break;
-                case MergeField.EstimatedEmployerDebt:
+                case MergeField.EstimatedEmployerDebtAt:
                     text = string.Format("Estimated Employer Debt as at {0} ", _dataAccess.GetCessationDate());
                     break;
                 case MergeField.IntroListItemOnePartOne:
@@ -257,6 +214,9 @@ namespace PDFCreator
                 case MergeField.IntroListItemSeven:
                     text = "The estimate provided in this document might not reflect the total amount that would be due if your organisation incurs a cessation event.  In particular: \n";
                     break;
+                case MergeField.EstimatedEmployerDebt:
+                    text = "Estimated Employer Debt";
+                    break;
                 default:
                     //oops
                     break;                
@@ -267,7 +227,7 @@ namespace PDFCreator
         private enum MergeField
         {
             Title,
-            EstimatedEmployerDebt,
+            EstimatedEmployerDebtAt,
             IntroListItemOnePartOne,
             IntroListItemOnePartTwo,
             IntroListItemTwo,
@@ -275,7 +235,41 @@ namespace PDFCreator
             IntroListItemFour,
             IntroListItemFive,
             IntroListItemSix,
-            IntroListItemSeven
+            IntroListItemSeven,
+            EstimatedEmployerDebt
+        }
+
+        private void NewPage()
+        {
+            _document.Add(new AreaBreak(AreaBreakType.NEXT_PAGE));
+        }
+
+        private void AddLogo()
+        {
+            //logo            
+            string strFilePath = @"\\bbs-actuaries\dfsdata\users\hodsonl\Visual Studio 2017\Projects\PDFCreator\PDFCreator\Images\baptist.png";
+            ImageData l = ImageDataFactory.Create(strFilePath);
+            Image img = new Image(l);
+            img.SetHeight(39);
+            img.SetWidth(110);
+            img.SetFixedPosition(_pageWidth - 180, _pageHeight - 65);
+            _document.Add(img);
+        }
+
+        private void AddTitle(string titleText)
+        {
+            Paragraph title = new Paragraph(titleText);
+            _titleParaHeight = 126;
+            _titleParaWidth = _pageWidth;
+            title.SetFontSize(13);
+            title.SetBold();
+            title.SetTextAlignment(TextAlignment.CENTER);
+            title.SetFontColor(_baptistBlue);
+            title.SetUnderline();
+            title.SetWidth(_titleParaWidth);
+            title.SetHeight(_titleParaHeight);            
+            title.SetFixedPosition((_pageWidth - _titleParaWidth), _verticalPosition, _titleParaWidth);
+            _document.Add(title);
         }
         #endregion
     }
